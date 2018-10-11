a07706a57f1cb91005d9600bbbed02bb73d21f87
louismn <louis.martindunord@eemi.com>
2018-10-11 12:49:25 +0200

    Update de l'exo 1

--- 00_exo_jeudi.rb
+++ 00_exo_jeudi.rb
@@ -3,37 +3,36 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-url_start = "http://annuaire-des-mairies.com"
-
+@url_start = 'http://annuaire-des-mairies.com'
 
 def get_the_email_of_a_townhal_from_its_webpage(url)
-
-   page = Nokogiri::HTML(open(url))
-    p page.css('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
-
-
+  page = Nokogiri::HTML(open(url))
+  p page.css('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
 end
 
-
-get_the_email_of_a_townhal_from_its_webpage("http://annuaire-des-mairies.com/95/champagne-sur-oise.html")
-
+get_the_email_of_a_townhal_from_its_webpage('http://annuaire-des-mairies.com/95/andilly.html')
 
 
 
 
 def get_all_the_urls_of_val_doise_townhalls
-	 
-	 page= Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
-	 links = page.css("a").select{ |link| link['class'] == "lientxt"}
-	 links.each {|link| print link["href"]}
-	
-	
+  @array_link = []
 
+  page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
+  links = page.css('a').select { |link| link['class'] == 'lientxt' }
+  links.each { |link| @array_link.push @url_start + link['href']}
+  puts @array_link
 
-#binding.pry
 end
-
-
 
 get_all_the_urls_of_val_doise_townhalls
 
+
+
+=begin
+def perform
+ get_the_email_of_a_townhal_from_its_webpage(get_all_the_urls_of_val_doise_townhalls)
+  end
+
+perform
+=end
