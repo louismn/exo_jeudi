8418f32e5d4f73610879786d16ac14c894c790cd
beeant8 <pdelatour7@gmail.com>
2018-10-11 10:36:43 +0200

    update exo 1

--- 00_exo_jeudi.rb
+++ 00_exo_jeudi.rb
@@ -1,11 +1,13 @@
 require 'rubygems'
 require 'nokogiri'
-require "open_uri"
-
+require 'open-uri'
 def get_the_email_of_a_townhal_from_its_webpage
 
-	page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/95/vaureal.html'))
+   page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/95/vaureal.html'))
+    p page.css('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
 
-	puts page.class ("lientxt2")
+
 
 end
+
+get_the_email_of_a_townhal_from_its_webpage
